25c4728f969a95631fcab41a5b551964c468719d
crd <chenrd57@gmail.com>
2018-04-16 14:33:55 +0800

    version:0.2

--- LDOPE_str.py
+++ LDOPE_str.py
@@ -9,22 +9,29 @@ import numpy as np
 xl = lambda x: 16 * x + 1
 n = np.arange(22)
 days = xl(n)
-with open('D:/NDVI/batch.txt', 'w') as file_object:
+with open('D:/NDVI/batch.cmd', 'w') as file_object:
     for year in range(2000, 2018):
         for day in enumerate(days):
-            if day[1] < 10:
-                strs = (
-                        "unpack_sds_bits MOD13Q1.A%d00%d.hdf -sds= \"250m 16 days VI Quality\" -bit=0-3 "
-                        "-of=MOD13Q1_A%d00%d.hdf \n" %
-                        (year, day[1], year, day[1]))
-            if 10 <= day[1] < 100:
-                strs = (
-                        "unpack_sds_bits MOD13Q1.A%d0%d.hdf -sds= \"250m 16 days VI Quality\" -bit=0-3 "
-                        "-of=MOD13Q1_A%d0%d.hdf \n" %
-                        (year, day[1], year, day[1]))
-            if day[1] >= 100:
-                strs = (
-                        "unpack_sds_bits MOD13Q1.A%d%d.hdf -sds= \"250m 16 days VI Quality\" -bit=0-3 "
-                        "-of=MOD13Q1_A%d%d.hdf \n" %
-                        (year, day[1], year, day[1]))
-            file_object.write(strs)
+            for nn in range(25, 27):
+                if day[1] < 10:
+                    strs = (
+                        "unpack_sds_bits MOD13Q1.A%d00%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "-of=MOD13Q1_A%d00%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d00%d.h%dv05.006.hdf "
+                        "MOD13Q1_A%d00%d.h%dv05.006.hdf -of=MOD13Q1_A%d00%d.h%dv05.006_unpack.hdf\ndel "
+                        "MOD13Q1_A%d00%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
+                                                            day[1], nn, year, day[1], nn, year, day[1], nn))
+                if 10 <= day[1] < 100:
+                    strs = (
+                        "unpack_sds_bits MOD13Q1.A%d0%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "-of=MOD13Q1_A%d0%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d0%d.h%dv05.006.hdf "
+                        "MOD13Q1_A%d0%d.h%dv05.006.hdf -of=MOD13Q1_A%d0%d.h%dv05.006_unpack.hdf\ndel "
+                        "MOD13Q1_A%d0%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
+                                                            day[1], nn, year, day[1], nn, year, day[1], nn))
+                if day[1] >= 100:
+                    strs = (
+                        "unpack_sds_bits MOD13Q1.A%d%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "-of=MOD13Q1_A%d%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d%d.h%dv05.006.hdf "
+                        "MOD13Q1_A%d%d.h%dv05.006.hdf -of=MOD13Q1_A%d%d.h%dv05.006_unpack.hdf\ndel "
+                        "MOD13Q1_A%d%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
+                                                            day[1], nn, year, day[1], nn, year, day[1], nn))
+                file_object.write(strs)
