8c5bfcd7ca609078d6f56855af1125f267d99761
crd <chenrd57@gmail.com>
2018-04-16 22:15:11 +0800

    version:0.2

--- LDOPE_str.py
+++ LDOPE_str.py
@@ -15,21 +15,21 @@ with open('D:/NDVI/batch.cmd', 'w') as file_object:
             for nn in range(25, 27):
                 if day[1] < 10:
                     strs = (
-                        "unpack_sds_bits MOD13Q1.A%d00%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "unpack_sds_bits MOD13Q1.A%d00%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-1 "
                         "-of=MOD13Q1_A%d00%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d00%d.h%dv05.006.hdf "
                         "MOD13Q1_A%d00%d.h%dv05.006.hdf -of=MOD13Q1_A%d00%d.h%dv05.006_unpack.hdf\ndel "
                         "MOD13Q1_A%d00%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
                                                             day[1], nn, year, day[1], nn, year, day[1], nn))
                 if 10 <= day[1] < 100:
                     strs = (
-                        "unpack_sds_bits MOD13Q1.A%d0%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "unpack_sds_bits MOD13Q1.A%d0%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-1 "
                         "-of=MOD13Q1_A%d0%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d0%d.h%dv05.006.hdf "
                         "MOD13Q1_A%d0%d.h%dv05.006.hdf -of=MOD13Q1_A%d0%d.h%dv05.006_unpack.hdf\ndel "
                         "MOD13Q1_A%d0%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
                                                             day[1], nn, year, day[1], nn, year, day[1], nn))
                 if day[1] >= 100:
                     strs = (
-                        "unpack_sds_bits MOD13Q1.A%d%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-3 "
+                        "unpack_sds_bits MOD13Q1.A%d%d.h%dv05.006.hdf -sds=\"250m 16 days VI Quality\" -bit=0-1 "
                         "-of=MOD13Q1_A%d%d.h%dv05.006.hdf\ncp_proj_param -ref=MOD13Q1.A%d%d.h%dv05.006.hdf "
                         "MOD13Q1_A%d%d.h%dv05.006.hdf -of=MOD13Q1_A%d%d.h%dv05.006_unpack.hdf\ndel "
                         "MOD13Q1_A%d%d.h%dv05.006.hdf\n" % (year, day[1], nn, year, day[1], nn, year, day[1], nn, year,
